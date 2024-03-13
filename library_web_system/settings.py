faf4d8df4d74a1cb3a05f2d08a7ddd6e915b0d99
Nelliosis <jcapawing@gmail.com>
2024-03-13 21:03:46 +0800

    allow all hosts

--- library_web_system/settings.py
+++ library_web_system/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEV")
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','library-dflpl.onrender.com','danielfajardolibrary.online','smtp.hostinger.com','.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
